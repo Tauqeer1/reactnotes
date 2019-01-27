d5fc97c8976cce5a5fe0833dcc8128570ec4b24e
Tauqeer Ahmed Shakir <tauqeer.shakir@yahoo.com>
2019-01-28 00:27:27 +0500

    add firebase config

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import Note from "./Note/Note";
 import NoteForm from "./NoteForm/NoteForm";
+import { config } from "./Config/config";
+import firebase from "firebase/app";
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    this.app = firebase.initializeApp(config);
+    this.db = this.app
+      .database()
+      .ref()
+      .child("notes");
     // We are going to setup the React state of our component
     this.state = {
-      notes: [
-        { id: 1, noteContent: "Note 1 here!" },
-        { id: 2, noteContent: "Note 2 here!" }
-      ]
+      notes: []
     };
 
     this.addNote = this.addNote.bind(this);
---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyDc2Ios045_ZrOwiPIpP0pV1qPP_qX9YN4",
+  authDomain: "fir-78423.firebaseapp.com",
+  databaseURL: "https://fir-78423.firebaseio.com",
+  projectId: "fir-78423",
+  storageBucket: "fir-78423.appspot.com",
+  messagingSenderId: "529863196492"
+};
